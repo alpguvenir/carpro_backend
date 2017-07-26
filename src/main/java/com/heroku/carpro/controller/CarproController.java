83fd182d5d584c7a2c282c85fc414c5f721de37c
alpguvenir <alpguvenir@gmail.com>
2017-07-26 16:01:52 +0300

    constructor + service annotation

--- src/main/java/com/heroku/carpro/controller/CarproController.java
+++ src/main/java/com/heroku/carpro/controller/CarproController.java
@@ -1,14 +1,14 @@
 package com.heroku.carpro.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.heroku.carpro.service.CarproService;
 
-@RestController
+@Controller
 public class CarproController {
 	
 	private CarproService carproService;
@@ -18,6 +18,12 @@ public class CarproController {
 		this.carproService = carproService;
 	}
 	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+	
 	@GetMapping("/cars")
 	@ResponseBody
 	public String getAllCars() {
@@ -25,10 +31,6 @@ public class CarproController {
 	}
 	
 	
-	@RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
+	
 	
 }
--- src/main/java/com/heroku/carpro/service/CarproService.java
+++ src/main/java/com/heroku/carpro/service/CarproService.java
@@ -1,5 +1,8 @@
 package com.heroku.carpro.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CarproService {
 
 }
