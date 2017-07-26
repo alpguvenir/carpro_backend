2cfc08d301bfe3d79b950da6b013bdfb63b3043c
alpguvenir <alpguvenir@gmail.com>
2017-07-26 15:48:41 +0300

    including the service layer

--- src/main/java/com/heroku/carpro/controller/CarproController.java
+++ src/main/java/com/heroku/carpro/controller/CarproController.java
@@ -1,13 +1,19 @@
 package com.heroku.carpro.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.heroku.carpro.service.CarproService;
+
 @Controller
 public class CarproController {
 
+	@Autowired
+	private CarproService carproService;
+	
 	@GetMapping("/cars")
 	@ResponseBody
 	public String getAllCars() {
---
+++ src/main/java/com/heroku/carpro/service/CarproService.java
@@ -0,0 +1,5 @@
+package com.heroku.carpro.service;
+
+public class CarproService {
+
+}
