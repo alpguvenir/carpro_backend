bfe41c610de14a20c5cfc9e2faae3796a126c594
alpguvenir <alpguvenir@gmail.com>
2017-07-26 15:55:18 +0300

    controller to rest controller

--- src/main/java/com/heroku/carpro/controller/CarproController.java
+++ src/main/java/com/heroku/carpro/controller/CarproController.java
@@ -1,14 +1,14 @@
 package com.heroku.carpro.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.heroku.carpro.service.CarproService;
 
-@Controller
+@RestController
 public class CarproController {
 	
 	@Autowired
