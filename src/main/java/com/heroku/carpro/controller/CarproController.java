29024deb8bba5865d553afff4644c5e43c9c064f
alpguvenir <alpguvenir@gmail.com>
2017-07-26 16:18:35 +0300

    getAllCars()

--- src/main/java/com/heroku/carpro/controller/CarproController.java
+++ src/main/java/com/heroku/carpro/controller/CarproController.java
@@ -1,11 +1,14 @@
 package com.heroku.carpro.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.heroku.carpro.entity.Carpro;
 import com.heroku.carpro.service.CarproService;
 
 @Controller
@@ -18,7 +21,7 @@ public class CarproController {
 		this.carproService = carproService;
 	}
 	
-	@RequestMapping("/")
+	@GetMapping("/")
     @ResponseBody
     String home() {
       return "Hello World!";
@@ -26,8 +29,14 @@ public class CarproController {
 	
 	@GetMapping("/cars")
 	@ResponseBody
-	public String getAllCars() {
+	public String getCars() {
 		return "Cars a dair hersey";
+	}
+	
+	@GetMapping("/getAllCars")
+	@ResponseBody
+	public List<Carpro> getAllCars() {
+		return this.carproService.getAllCars();
 	}
 	
 	
--- src/main/java/com/heroku/carpro/service/CarproService.java
+++ src/main/java/com/heroku/carpro/service/CarproService.java
@@ -1,8 +1,12 @@
 package com.heroku.carpro.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.heroku.carpro.entity.Carpro;
 import com.heroku.carpro.repository.CarproRepository;
 
 @Service
@@ -13,6 +17,12 @@ public class CarproService {
 	@Autowired
 	public CarproService (CarproRepository carproRepository) {
 		this.carproRepository = carproRepository;
+	}
+
+	public List<Carpro> getAllCars() {
+		List<Carpro> cars = new ArrayList<>();
+		carproRepository.findAll().forEach(cars::add);
+		return cars;
 	}
 	
 }
