7b67869d0b62ee68368bae7581c6c71c8b9b659c
alpguvenir <alpguvenir@gmail.com>
2017-07-26 16:29:46 +0300

    postgresql conn

--- src/main/java/com/heroku/carpro/CarproApplication.java
+++ src/main/java/com/heroku/carpro/CarproApplication.java
@@ -13,3 +13,10 @@ public class CarproApplication {
 		SpringApplication.run(CarproApplication.class, args);
 	}
 }
+
+/*
+C:\Users\Administrator\Desktop\carpro>heroku config
+=== fierce-tor-67008 Config Vars
+DATABASE_URL: postgres://liquyobugrkwmp:b5b6a87692f7c8184fdd9e131c094e4a97c753338a0f168c6f4ca03198c0e182@ec2-184-73-199-72.compute-1.amazonaws.com:5432/d2dphbb17ob2hc
+
+*/
