6910a7007461e83c0add25409995c26d2ba5ed9a
alpguvenir <alpguvenir@gmail.com>
2017-07-26 15:44:14 +0300

    deneme3

--- src/main/java/com/heroku/carpro/controller/CarproController.java
+++ src/main/java/com/heroku/carpro/controller/CarproController.java
@@ -1,12 +1,19 @@
 package com.heroku.carpro.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CarproController {
 
+	@GetMapping("/cars")
+	public String getAllCars() {
+		return "Cars a dair hersey";
+	}
+	
+	
 	@RequestMapping("/")
     @ResponseBody
     String home() {
