593e60c6eefd49b2ce6093e2c8b8659603ae08ac
alpguvenir <alpguvenir@gmail.com>
2017-07-26 16:06:01 +0300

    repository included

---
+++ src/main/java/com/heroku/carpro/repository/CarproRepository.java
@@ -0,0 +1,8 @@
+package com.heroku.carpro.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CarproRepository {
+
+}
--- src/main/java/com/heroku/carpro/service/CarproService.java
+++ src/main/java/com/heroku/carpro/service/CarproService.java
@@ -1,8 +1,18 @@
 package com.heroku.carpro.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.heroku.carpro.repository.CarproRepository;
 
 @Service
 public class CarproService {
 
+	private CarproRepository carproRepository;
+	
+	@Autowired
+	public CarproService (CarproRepository carproRepository) {
+		this.carproRepository = carproRepository;
+	}
+	
 }
