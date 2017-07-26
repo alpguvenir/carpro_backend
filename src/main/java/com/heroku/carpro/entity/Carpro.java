7a4f5b0207f3454c7fd854c61869ba8e054d3535
alpguvenir <alpguvenir@gmail.com>
2017-07-26 16:09:10 +0300

    entity included and repository is an interface check

---
+++ src/main/java/com/heroku/carpro/entity/Carpro.java
@@ -0,0 +1,37 @@
+package com.heroku.carpro.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+// Telling that Vehicle class is an Entity in my relational DB
+@Entity
+// The getters and setter are created with lombok
+// lombok.jar is included into the project under referenced libraries
+// Also lombok is included as a dependency in pom.xml
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Carpro {
+
+	// Telling that this is a primary key that is auto incremented
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String brand;
+	private int yearEstablished;
+	
+	public int compareTo(Carpro carpro) {
+		return id - carpro.getId();
+	}
+	
+}
--- src/main/java/com/heroku/carpro/repository/CarproRepository.java
+++ src/main/java/com/heroku/carpro/repository/CarproRepository.java
@@ -3,6 +3,6 @@ package com.heroku.carpro.repository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CarproRepository {
+public interface CarproRepository {
 
 }
